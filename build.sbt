116edb7a2cfa04a31f9d295a7c3a807d71b0badc
Ethan Winograd <ethanwinograd@gmail.com>
2015-04-30 13:01:31 -0400

    holy fishdog

--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ scalaVersion := "2.10.4"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
 
-libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % "test"
+libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "3.2.1" % "test"
 
 
 libraryDependencies += "org.gitlab" % "java-gitlab-api" % "1.1.4"
--- src/test/scala/GitRepoAccessorSpec.scala
+++ src/test/scala/GitRepoAccessorSpec.scala
@@ -1,6 +1,12 @@
+import java.util
+
+import org.giant.squid.GitRepoAccessor
+import org.gitlab.api.GitlabAPI
+import org.gitlab.api.models.GitlabProject
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.FunSuite
 
-class GitRepoAccessorSpec extends FunSuite {
+class GitRepoAccessorSpec extends FunSuite with MockFactory {
 
   test("An empty Set should have size 0") {
     assert(Set.empty.size == 0)
@@ -10,5 +16,13 @@ class GitRepoAccessorSpec extends FunSuite {
     intercept[NoSuchElementException] {
       Set.empty.head
     }
+  }
+
+
+  test("dogfish head"){
+
+      val apiMock = mock[GitlabAPI]
+      (apiMock.getAllProjects _).expects().returning(new util.ArrayList[GitlabProject])
+      assert(new GitRepoAccessor(apiMock).GetProjects().size == 0)
   }
 }
